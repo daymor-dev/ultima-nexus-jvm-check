46287162f4025f8063827932cfca6bb2f24949f9
daymor <malcolm.basket@hotmail.fr>
2025-12-28 16:30:09 +0900

    update publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.ultimanexus.feature.publish)
+    alias(libs.plugins.ultimanexus.feature.publishMavenCentral)
     alias(libs.plugins.ultimanexus.check.formatGradle)
     alias(libs.plugins.ultimanexus.base.lifecycle)
 }
